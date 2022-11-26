47264de164991786dc29f369c935c7ec360dd77a
fatemakhatun7 <fatema.sprdnc@gmail.com>
2022-11-26 15:48:53 +0600

    error

--- src/Pages/Routes/Routes/Routes.js
+++ src/Pages/Routes/Routes/Routes.js
@@ -1,12 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../../Layout/Main";
 import Blogs from "../../Blogs/Blogs";
+import Error from "../../Error/Error";
 import Home from "../../Home/Home/Home";
 
 const router = createBrowserRouter([
     {
       path: '/',
       element: <Main></Main>,
+      errorElement: <Error></Error>,
       children:[
         {
           path: '/',
